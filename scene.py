71d64794a14d7c9acd08fe2e658355934325a5a7
svpr1m <bigpapagg@icloud.com>
2022-06-01 14:51:44 +0545

    added 3d

--- scene.py
+++ scene.py
@@ -34,3 +34,35 @@ class Txt(Scene):
         self.play(Write(p_d.shift(1.35*LEFT + 0.5*DOWN)))
         self.play(Write(m.shift(2.5*LEFT + 1*DOWN)))
         self.play(Write(psi.shift(1.7*LEFT + 1.5*DOWN)))
+
+class ScalarField(ThreeDScene):
+    def construct(self):
+        title = Text("Trying out 3D Graphs", color=GREEN, font="Noto Sans")
+        
+        axes = ThreeDAxes(x_range=[-4,4], x_length=4)
+
+        func = Surface(
+            lambda u, v: axes.c2p(*self.func(u, v)),
+            u_range=[-PI, PI],
+            v_range=[0, TAU]
+        ) 
+
+        self.play(Write(title))
+        self.wait(1)
+
+        self.play(Uncreate(title))
+
+        self.move_camera(0.8*np.pi / 2, -0.45 * np.pi)
+
+        self.play(Write(axes))
+        self.play(Write(func))
+        self.begin_ambient_camera_rotation()
+        self.wait(4)
+
+    @staticmethod
+    def func(u,v):
+       return np.array([
+            u,
+            v,
+            np.sin(5*u)*(np.cos(5*v))/5
+        ])
